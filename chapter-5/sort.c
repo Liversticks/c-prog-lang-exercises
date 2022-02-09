0cee4eef57f00abad1fc425caaedbff781a0fe50
oxie <oliver_xie@sfu.ca>
2022-02-09 14:09:15 -0800

    exercise 5-16

--- chapter-5/sort.c
+++ chapter-5/sort.c
@@ -10,11 +10,14 @@ void freelines(char** lineptr, size_t nlines);
 void my_qsort(void * lineptr[], int left, int right, bool increasing, int (*comp)(void*, void*));
 int numcmp(char*, char*);
 int foldcmp(char*, char*);
+int dircmp_fold(char *, char *);
+int dircmp_nofold(char *, char *);
 
 int main(int argc, char** argv) {
     bool numeric = false;
     bool increasing = true;
     bool fold = false;
+    bool dir = false;
 
     if (argc > 1) {
         for (int i = 1; i < argc; i++) {
@@ -29,14 +32,18 @@ int main(int argc, char** argv) {
                     case 'f':
                         fold = true;
                         break;
+                    case 'd':
+                        dir = true;
+                        break;
                 }
             }
         }
     }
     size_t nlines = 0;
     char** lineptr = readlines(&nlines);
+    // directory will override numeric
     // numeric will override fold if both are specified
-    my_qsort((void**)lineptr, 0, nlines-1, increasing, (int (*)(void*, void*))(numeric ? numcmp : (fold ? foldcmp : strcmp)));
+    my_qsort((void**)lineptr, 0, nlines-1, increasing, (int (*)(void*, void*))(dir ? (fold ? dircmp_fold : dircmp_nofold) : (numeric ? numcmp : (fold ? foldcmp : strcmp))));
     writelines(lineptr, nlines);
     freelines(lineptr, nlines);
     return 0;
@@ -84,6 +91,90 @@ int foldcmp(char * s1, char * s2) {
     free(s1_temp);
     free(s2_temp);
     return ret;
+}
+
+int dircmp_nofold(char * s1, char * s2) {
+    size_t s1_len = strlen(s1);
+    size_t s2_len = strlen(s2);
+    size_t cmplen = s1_len > s2_len ? s2_len : s1_len;
+    size_t i;
+    for (i = 0; i < cmplen; i++) {
+        if (s1[i] != s2[i]) {
+            // order: spaces < numbers < letters
+            if (isspace(s1[i])) {
+                if (isspace(s2[i])) {
+                    return s1[i] - s2[i];    
+                }
+                return -1;
+            }
+            else if (isdigit(s1[i])) {
+                if (isspace(s2[i])) {
+                    return 1;
+                }
+                else if (isdigit(s2[i])) {
+                    return s1[i] - s2[i];
+                }
+                return -1;
+            }
+            else if (isalpha(s1[i])) {
+                if (isspace(s2[i]) || isdigit(s2[i])) {
+                    return -1;
+                }
+                else if (isalpha(s1[i])) {
+                    return s1[i] - s2[i];
+                }
+                return -1;
+            }
+            else {
+                return s1[i] - s2[i];
+            }
+        }
+    }
+    if (s1_len > s2_len) return 1;
+    else if (s2_len > s1_len) return -1;
+    return 0;
+}
+
+int dircmp_fold(char * s1, char * s2) {
+    size_t s1_len = strlen(s1);
+    size_t s2_len = strlen(s2);
+    size_t cmplen = s1_len > s2_len ? s2_len : s1_len;
+    size_t i;
+    for (i = 0; i < cmplen; i++) {
+        if (s1[i] != s2[i]) {
+            // order: spaces < numbers < letters
+            if (isspace(s1[i])) {
+                if (isspace(s2[i])) {
+                    return s1[i] - s2[i];    
+                }
+                return -1;
+            }
+            else if (isdigit(s1[i])) {
+                if (isspace(s2[i])) {
+                    return 1;
+                }
+                else if (isdigit(s2[i])) {
+                    return s1[i] - s2[i];
+                }
+                return -1;
+            }
+            else if (isalpha(s1[i])) {
+                if (isspace(s2[i]) || isdigit(s2[i])) {
+                    return -1;
+                }
+                else if (isalpha(s1[i])) {
+                    return tolower(s1[i]) - tolower(s2[i]);
+                }
+                return -1;
+            }
+            else {
+                return s1[i] - s2[i];
+            }
+        }
+    }
+    if (s1_len > s2_len) return 1;
+    else if (s2_len > s1_len) return -1;
+    return 0;
 }
 
 int numcmp(char * s1, char * s2) {
